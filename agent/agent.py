a0a3d5706d01718937c6fc5122903a105d11b014
365codix <365codix@gmail.com>
2026-05-28 13:06:50 -0500

    Update agent.py

--- agent/agent.py
+++ agent/agent.py
@@ -14,7 +14,7 @@ async def entrypoint(ctx: JobContext):
 
     # Iniciar el modelo Multimodal de Gemini Flash (Ultra rápido y económico)
     model = google.beta.realtime.RealtimeModel(
-        model="models/gemini-2.0-flash-exp",
+        model="models/gemini-2.0-flash",
         voice="Aoede", # Opciones: Puck, Charon, Kore, Fenrir, Aoede
         temperature=0.7,
         instructions="Eres Civix, la asistente virtual de la central de emergencias. Escucha al ciudadano, averigua qué emergencia tiene y dile que un operador humano tomará su llamada de inmediato."
